f99412aba2ea493f61846f79b1f686f937f61000
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2025-08-06 14:34:03 -0300

    adicionando alguns metodos do sistema

--- ex02/script.js
+++ ex02/script.js
@@ -10,7 +10,19 @@ let estudos = {
     titulo: "Estudar",
     descricao: "Estudar para concurso",
     dataCriacao: new Date,
-    status: "em progesso",
+    status: "pendente",
+};
+let ingles = {
+    titulo: "Estudar",
+    descricao: "Entra em um curso de inglês",
+    dataCriacao: new Date,
+    status: "pendente"
+};
+let futebol = {
+    titulo: "Futebol",
+    descricao: "Voltar a jogar futebol",
+    dataCriacao: new Date,
+    status: "concluida"
 };
 function adicionarTarefa(tarefa, tarefas) {
     const newTask = {
@@ -22,8 +34,27 @@ function adicionarTarefa(tarefa, tarefas) {
 }
 console.log(adicionarTarefa(academia, tarefas));
 console.log(adicionarTarefa(estudos, tarefas));
-export {};
+console.log(adicionarTarefa(ingles, tarefas));
+console.log(adicionarTarefa(futebol, tarefas));
 // 3 - Crie uma função removerTarefa que remova uma tarefa pela id ou título.
+function removerTarefa(tarefas, idTask) {
+    const newListTaks = tarefas.filter(task => task.id !== idTask);
+    return newListTaks;
+}
+tarefas = removerTarefa(tarefas, 3);
+console.log(tarefas);
 // 4 - Crie uma função listarTarefas que exibe todas as tarefas.
+function listarTarefas(tarefas) {
+    const listTask = tarefas.map(task => task);
+    return listTask;
+}
+console.log(listarTarefas(tarefas));
 // 5 - Crie uma função filtrarTarefasPorStatus que filtre as tarefas por status (pendente, concluída, etc).
+function filtrarTarefasPorStatus(tarefas, status) {
+    const taskFiltered = tarefas.filter(task => task.status === status);
+    return taskFiltered;
+}
+let taskFilter = filtrarTarefasPorStatus(tarefas, "em progesso");
+console.log(taskFilter);
+export {};
 // 6 - Crie uma função alterarStatusTarefa que mude o status de uma tarefa.
--- ex02/script.ts
+++ ex02/script.ts
@@ -24,7 +24,21 @@ let estudos: Tarefa = {
    titulo: "Estudar",
    descricao: "Estudar para concurso",
    dataCriacao: new Date,
-   status: "em progesso",
+   status: "pendente",
+}
+
+let ingles: Tarefa = {
+   titulo: "Estudar",
+   descricao: "Entra em um curso de inglês",
+   dataCriacao: new Date,
+   status: "pendente"
+}
+
+let futebol: Tarefa = {
+   titulo: "Futebol",
+   descricao: "Voltar a jogar futebol",
+   dataCriacao: new Date,
+   status: "concluida"
 }
 
 function adicionarTarefa(tarefa : Tarefa, tarefas: Tarefa[]) : Tarefa[] {
@@ -42,15 +56,47 @@ function adicionarTarefa(tarefa : Tarefa, tarefas: Tarefa[]) : Tarefa[] {
 
 console.log(adicionarTarefa(academia, tarefas));
 console.log(adicionarTarefa(estudos, tarefas));
+console.log(adicionarTarefa(ingles, tarefas));
+console.log(adicionarTarefa(futebol, tarefas));
 
 
 // 3 - Crie uma função removerTarefa que remova uma tarefa pela id ou título.
 
+function removerTarefa(tarefas: Tarefa[], idTask: number) : Tarefa[] {
+
+   const newListTaks = tarefas.filter(task => task.id !== idTask)
+   
+   return newListTaks
+}
+
+tarefas = removerTarefa(tarefas, 3)
+
+console.log(tarefas)
+
 // 4 - Crie uma função listarTarefas que exibe todas as tarefas.
+
+function listarTarefas(tarefas : Tarefa[]) : Tarefa[] {
+
+   const listTask = tarefas.map(task => task) 
+
+   return listTask
+   
+}
+
+console.log(listarTarefas(tarefas));
 
 // 5 - Crie uma função filtrarTarefasPorStatus que filtre as tarefas por status (pendente, concluída, etc).
 
+function filtrarTarefasPorStatus(tarefas: Tarefa[], status: Status) : Tarefa[] {
+
+   const taskFiltered = tarefas.filter(task => task.status === status)
+
+   return taskFiltered
+}
+
+let taskFilter = filtrarTarefasPorStatus(tarefas, "em progesso")
+
+console.log(taskFilter);
+
+
 // 6 - Crie uma função alterarStatusTarefa que mude o status de uma tarefa.
-
-
-
