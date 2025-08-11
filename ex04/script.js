14510276d5fa5f8f983d94ae169865093710da4e
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2025-08-10 21:51:48 -0300

    starting the project of the system management library

---
+++ ex04/script.js
@@ -0,0 +1,13 @@
+// Desafio: Sistema de Gestão de Biblioteca
+export {};
+// Funcionalidades obrigatórias:
+// Adicionar livros à biblioteca.
+// Cadastrar usuários.
+// Emprestar livro para um usuário (apenas se o livro estiver disponível).
+// Devolver livro.
+// Listar livros disponíveis.
+// Listar livros emprestados de um usuário.
+// Regras extras:
+// Um usuário pode ter no máximo 3 livros emprestados ao mesmo tempo.
+// Ao emprestar um livro, o campo disponivel deve mudar para false.
+// Ao devolver, disponivel deve voltar para true.
