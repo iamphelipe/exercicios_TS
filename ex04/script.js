bc770b4f711d5b17218a0092bd623bd8ce6b427d
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2025-08-11 23:14:27 -0300

    finish the metod add book at the library

--- ex04/script.js
+++ ex04/script.js
@@ -1,7 +1,53 @@
 // Desafio: Sistema de Gestão de Biblioteca
-export {};
+let biblioteca = [
+    {
+        id: 1,
+        titulo: "A Branca de Neve",
+        autor: "Jakob Grimm",
+        anoPublicacao: 1812,
+        disponivel: true
+    },
+    {
+        id: 2,
+        titulo: "Pinóquio",
+        autor: "Carlo Collodi",
+        anoPublicacao: 1940,
+        disponivel: false
+    },
+    {
+        id: 3,
+        titulo: "O Garoto do Pijama Listrado",
+        autor: "John Boyne",
+        anoPublicacao: 2006,
+        disponivel: true
+    },
+    {
+        id: 4,
+        titulo: "Crepúsculo",
+        autor: "Stephenie Meyer",
+        anoPublicacao: 2008,
+        disponivel: false
+    }
+];
+console.log(biblioteca);
 // Funcionalidades obrigatórias:
 // Adicionar livros à biblioteca.
+function adicionarLivro(biblioteca, livro) {
+    const newBook = {
+        id: biblioteca.length + 1,
+        ...livro
+    };
+    biblioteca.push(newBook);
+    return biblioteca;
+}
+let reiLeao = {
+    titulo: "O Rei Leão",
+    autor: "Chris Sanders",
+    anoPublicacao: 1994,
+    disponivel: false
+};
+biblioteca = adicionarLivro(biblioteca, reiLeao);
+export {};
 // Cadastrar usuários.
 // Emprestar livro para um usuário (apenas se o livro estiver disponível).
 // Devolver livro.
--- ex04/script.ts
+++ ex04/script.ts
@@ -3,7 +3,7 @@
 // Objetivo: Criar um programa em TypeScript que gerencie empréstimos de livros em uma biblioteca.
 
 interface Livro {
-   id: number;
+   id?: number;
    titulo: string;
    autor: string;
    anoPublicacao: number;
@@ -16,10 +16,63 @@ interface Usuario {
    livrosEmprestados: Livro[]
 }
 
+let biblioteca : Livro[] = [
+   {
+      id: 1,
+      titulo: "A Branca de Neve",
+      autor: "Jakob Grimm",
+      anoPublicacao: 1812,
+      disponivel: true 
+   },
+   {
+      id: 2,
+      titulo: "Pinóquio",
+      autor: "Carlo Collodi",
+      anoPublicacao: 1940,
+      disponivel: false
+   },
+   {
+      id: 3,
+      titulo: "O Garoto do Pijama Listrado",
+      autor: "John Boyne",
+      anoPublicacao: 2006,
+      disponivel: true
+   },
+   {
+      id: 4,
+      titulo: "Crepúsculo",
+      autor: "Stephenie Meyer",
+      anoPublicacao: 2008,
+      disponivel: false
+   }
+]
+
+console.log(biblioteca);
 
 // Funcionalidades obrigatórias:
 
 // Adicionar livros à biblioteca.
+
+function adicionarLivro(biblioteca: Livro[], livro: Livro) {
+   
+   const newBook = {
+      id: biblioteca.length + 1,
+      ...livro
+   }
+
+   biblioteca.push(newBook)
+
+   return biblioteca
+}
+
+let reiLeao: Livro = {
+   titulo: "O Rei Leão",
+   autor: "Chris Sanders",
+   anoPublicacao: 1994,
+   disponivel: false
+}
+
+biblioteca = adicionarLivro(biblioteca, reiLeao)
 
 // Cadastrar usuários.
 
