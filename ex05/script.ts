22de1491fd283181969db0412daeffb0a1675e4c
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2025-08-17 19:28:04 -0300

    finish the game pedra Papel tesoura

---
+++ ex05/script.ts
@@ -0,0 +1,58 @@
+type Poder = "pedra" | "papel" | "tesoura"
+
+function pedraPapelTesoura(poder: Poder){
+
+   let poderBot = Math.floor(Math.random() * 3)
+
+   // 0 = pedra
+   // 1 = papel
+   // 2 = tesoura
+
+   if(poderBot === 0) { //PEDRA
+
+      let poderBotString = "Pedra"
+
+      if(poder === "pedra"){
+         return console.log(`Bot: ${poderBotString} | Você: ${poder} || Empate!!!`)
+      }
+      if(poder === "papel") {
+         return console.log(`Bot: ${poderBotString} | Você: ${poder} || Você ganhou!`)
+      }
+      else {
+         return console.log(`Bot: ${poderBotString} | Você: ${poder} || Você perdeu!`)
+      }
+   }
+
+   if(poderBot === 1) { //PAPEL
+
+      let poderBotString = "Papel"
+
+      if(poder === "pedra"){
+         return console.log(`Bot: ${poderBotString} | Você: ${poder} || Você perdeu!`)
+      }
+      if(poder === "papel") {
+         return console.log(`Bot: ${poderBotString} | Você: ${poder} || Empate!!!`)
+      }
+      else {
+         return console.log(`Bot: ${poderBotString} | Você: ${poder} || Você ganhou!`)
+      }
+   }
+
+   if(poderBot === 2) {
+
+      let poderBotString = "Tesoura"
+
+      if(poder === "pedra"){
+         return console.log(`Bot: ${poderBotString} | Você: ${poder} || Você ganhou!`)
+      }
+      if(poder === "papel") {
+         return console.log(`Bot: ${poderBotString} | Você: ${poder} || Você perdeu!`)
+      }
+      else {
+         return console.log(`Bot: ${poderBotString} | Você: ${poder} || Empate!!!`)
+      }
+   }
+   
+}
+
+pedraPapelTesoura("tesoura")
