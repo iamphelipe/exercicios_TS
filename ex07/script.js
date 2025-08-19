b00ba72ef551de0020261e57831810720affd5c1
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2025-08-19 20:32:13 -0300

    sending firsts files

--- ex07/script.js
+++ ex07/script.js
@@ -1,7 +1,19 @@
-async function fetchAPI() {
-    const response = await fetch("https://data.origamid.dev/vendas");
+let dateInicio = document.querySelector(".dateInicio");
+let dateFinal = document.querySelector(".dateFinal");
+async function fetchAPI(url) {
+    const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    // console.log(data)
+    return data;
 }
-fetchAPI();
+async function displaySreen() {
+    let data = await fetchAPI("https://data.origamid.dev/vendas");
+    if (dateInicio) {
+        console.log(dateInicio.value);
+    }
+}
+displaySreen();
+if (dateInicio) {
+    dateInicio.addEventListener("input", displaySreen);
+}
 export {};
--- ex07/script.ts
+++ ex07/script.ts
@@ -1,7 +1,25 @@
-async function fetchAPI() {
-   const response = await fetch("https://data.origamid.dev/vendas")
+let dateInicio = document.querySelector<HTMLInputElement>(".dateInicio")
+let dateFinal = document.querySelector<HTMLInputElement>(".dateFinal")
+
+async function fetchAPI(url: string) {
+   const response = await fetch(url)
    const data = await response.json()
-   console.log(data)
+   // console.log(data)
+
+   return data
 }
 
-fetchAPI()
+
+async function displaySreen() {
+   let data = await fetchAPI("https://data.origamid.dev/vendas")
+   if(dateInicio) {
+            console.log(dateInicio.value);
+   }
+   
+}
+
+displaySreen()
+
+if(dateInicio){
+   dateInicio.addEventListener("input", displaySreen)
+}
