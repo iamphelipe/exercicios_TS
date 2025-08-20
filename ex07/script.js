873ac65beeb8d91eb3e5cb7b72aa3b0e66eb2310
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2025-08-19 21:34:53 -0300

    finish the project

--- ex07/script.js
+++ ex07/script.js
@@ -1,19 +1,29 @@
 let dateInicio = document.querySelector(".dateInicio");
 let dateFinal = document.querySelector(".dateFinal");
+let result = document.querySelector("div");
 async function fetchAPI(url) {
     const response = await fetch(url);
     const data = await response.json();
-    // console.log(data)
     return data;
 }
 async function displaySreen() {
-    let data = await fetchAPI("https://data.origamid.dev/vendas");
-    if (dateInicio) {
-        console.log(dateInicio.value);
+    if (dateInicio && dateFinal && result) {
+        if (dateInicio.value === "" || dateFinal.value === "")
+            return;
+        let inicio = dateInicio.value;
+        let final = dateFinal.value;
+        let data = await fetchAPI(`https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`);
+        console.log(data);
+        data.map(d => {
+            result.innerHTML += `
+         
+         `;
+        });
     }
 }
 displaySreen();
-if (dateInicio) {
+if (dateInicio && dateFinal) {
     dateInicio.addEventListener("input", displaySreen);
+    dateFinal.addEventListener("input", displaySreen);
 }
 export {};
